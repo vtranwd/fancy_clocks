e386760a38f92efa1918df8c2cf242f4d54b6b36
Victor Tran <vtranwd@gmail.com>
2021-09-17 15:01:48 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const http = require('http');
+
+const clocks = [];
+
+const createClock = (res) => {
+  const myTimer = setInterval(function(){ alert("Hello"); }, 1000);
+  clocks.push();
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    data: 'I will create a clock',
+  }));
+};
+
+const getClock = (res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    data: 'getting clocks',
+  }));
+};
+
+const yellAtClient = (res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    data: 'ya cunt!'
+  }));
+};
+
+// Create a local server to receive data from
+const server = http.createServer((req, res) => {
+  switch(req.url) {
+    case "/createClock":
+      createClock(res);
+      break;
+    case "/getClock":
+      getClock(res);
+      break;
+    default:
+      yellAtClient(res);
+  }
+});
+
+server.listen(8000);
+
